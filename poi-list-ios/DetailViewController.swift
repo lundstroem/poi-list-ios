8365884c9b9823599ac6495bbe3f37b9dd354e43
Harry Lundström <h.lundstroem@gmail.com>
2017-02-12 14:06:29 +0100

    init commit

---
+++ poi-list-ios/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  poi-list-ios
+//
+//  Created by Harry Lundstrom on 12/02/17.
+//  Copyright © 2017 Harry Lundström. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
---
+++ poi-list-iosTests/poi_list_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  poi_list_iosTests.swift
+//  poi-list-iosTests
+//
+//  Created by Harry Lundstrom on 12/02/17.
+//  Copyright © 2017 Harry Lundström. All rights reserved.
+//
+
+import XCTest
+@testable import poi_list_ios
+
+class poi_list_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
